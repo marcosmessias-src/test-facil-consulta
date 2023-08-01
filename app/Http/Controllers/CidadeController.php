ae42479f249bf21c45c2e183f6461ec9091b62b8
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 15:39:50 -0300

    Criado o método paraa listagem de todas as cidades.

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CidadeResource;
+use App\Models\Cidade;
 use Illuminate\Http\Request;
 
 class CidadeController extends Controller
 {
-    //
+    /**
+     * List all cities.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return CidadeResource::collection(Cidade::all());
+    }
 }
