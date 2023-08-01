23efc04c635f710aad6a556a955c33e46ea1c347
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 11:08:30 -0300

    Definido atributos do Model Médico + SoftDelete

--- app/Models/Medico.php
+++ app/Models/Medico.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Medico extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+        'especialidade',
+        'cidade_id'
+    ];
+
+    /**
+     * SoftDelete addition.
+     */
+    protected $dates = ['deleted_at'];
 }
