fdbfa24145f9ee1f69b379f35e4f61d2f86c0b14
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 15:42:15 -0300

    Otimizada a rota register.

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
     Route::post('login', 'login');
     Route::get('user', 'user');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 });
-
-Route::post('register', [AuthController::class, 'register']);
 
 /*
 |--------------------------------------------------------------------------
