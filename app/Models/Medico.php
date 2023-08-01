1ae0441ac9f05064a4384196c912b2b9bd80a2c4
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 14:29:45 -0300

    Implementado validação de input do Model Médico.

--- app/Models/Medico.php
+++ app/Models/Medico.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
 
 class Medico extends Model
 {
@@ -25,4 +26,22 @@ class Medico extends Model
      * SoftDelete addition.
      */
     protected $dates = ['deleted_at'];
+
+    /**
+     * Rules for validation.
+     */
+    protected $rules = array(
+        'nome' => 'required|max:100',
+        'especialidade' => 'required|max:100'
+    );
+
+    /**
+     * Input validation.
+     */
+    public function validate($inputs){
+        $validator = Validator::make($inputs, $this->rules);
+        if ($validator->passes()) return true;
+        $this->errors = $validator->messages();
+        return false;
+    }
 }
