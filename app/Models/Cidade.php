29bc6a963aa2bb6d7faf62ca799803a131108e4c
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 14:29:33 -0300

    Implementado validação de input do Model Cidade.

--- app/Models/Cidade.php
+++ app/Models/Cidade.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
 
 class Cidade extends Model
 {
@@ -24,4 +25,22 @@ class Cidade extends Model
      * SoftDelete addition.
      */
     protected $dates = ['deleted_at'];
+
+    /**
+     * Rules for validation.
+     */
+    protected $rules = array(
+        'nome' => 'required|max:100',
+        'estado' => 'required|max:100'
+    );
+
+    /**
+     * Input validation.
+     */
+    public function validate($inputs){
+        $validator = Validator::make($inputs, $this->rules);
+        if ($validator->passes()) return true;
+        $this->errors = $validator->messages();
+        return false;
+    }
 }
