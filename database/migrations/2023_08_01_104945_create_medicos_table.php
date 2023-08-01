8a30d8cfcd15d88bd980614135946d9d3f3f90ad
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-01 11:22:52 -0300

    Adicionado atributos à Migration de Médicos.

--- database/migrations/2023_08_01_104945_create_medicos_table.php
+++ database/migrations/2023_08_01_104945_create_medicos_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('medicos', function (Blueprint $table) {
             $table->id();
+            $table->text('nome');
+            $table->text('especialidade');
+            $table->foreignId('cidade_id')->constrained();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
