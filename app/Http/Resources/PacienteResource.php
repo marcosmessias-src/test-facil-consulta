f6e5c0dbd94adf92d9bca3890e79de501f3fbb0f
Marcos Messias <marcosmessias1998@gmail.com>
2023-08-02 02:08:04 -0300

    Alterado o retorno de datas nos resources.

--- app/Http/Resources/PacienteResource.php
+++ app/Http/Resources/PacienteResource.php
@@ -19,9 +19,9 @@ class PacienteResource extends JsonResource
             'nome' => $this->nome,
             'cpf' => $this->cpf,
             'celular' => $this->celular,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'deleted_at' => $this->deleted_at
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'deleted_at' => $this->deleted_at->format('Y-m-d H:i:s')
         ];
     }
 }
--- app/Http/Resources/StoreMedicoResource.php
+++ app/Http/Resources/StoreMedicoResource.php
@@ -19,9 +19,9 @@ class StoreMedicoResource extends JsonResource
             'nome' => $this->nome,
             'especialidade' => $this->especialidade,
             'cidade_id' => $this->cidade_id,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'deleted_at' => $this->deleted_at
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'deleted_at' => $this->deleted_at->format('Y-m-d H:i:s')
         ];
     }
 }
